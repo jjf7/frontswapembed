0c56a7b81cb399889da5bbeb03558ca82e8d3120
jjf7 <jfsistemas.contacto@gmail.com>
2021-12-22 14:27:27 -0400

    swap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+const styles = {
+  wrapper: {
+    width: "70%",
+    height:"550px",
+    paddingTop: "56.25",
+  },
+  responsiveIframe: {
+    top: "0",
+    left: "0",
+    bottom: "0",
+    right: "0",
+    width: "100%",
+    height: "100%",
+  },
+};
+
+const Iframe = ({children}) => {
+  return (
+    <div style={styles.wrapper}>
+      <label style={{fontWeight: "700"}}>{children}</label>
+      <iframe title="swap"
+        style={styles.responsiveIframe}
+        src="https://pancakeswapembed.vercel.app/"
+      ></iframe>
+    </div>
+  );
+};
+
 function App() {
   return (
     <>
@@ -10,10 +38,12 @@ function App() {
           </a>
         </div>
       </nav>
-      <div className="container">
+      <div className="container pt-5">
         <div className="row">
           <div className="col-md-6">Texto</div>
-          <div className="col-md-6">Swap</div>
+          <div className="col-md-6">
+            <Iframe>Intercambia tokens</Iframe>
+          </div>
         </div>
       </div>
     </>
