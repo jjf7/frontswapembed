9c9b86b48cd816ded587f8af4bf0971e9cdeb206
jjf7 <jfsistemas.contacto@gmail.com>
2021-12-22 12:35:58 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">Texto</div>
+        <div className="col-md-6">Swap</div>
+      </div>
     </div>
   );
 }
