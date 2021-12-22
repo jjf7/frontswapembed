196388ff8949a9e15fd600458c06d91b8ba03f3a
jjf7 <jfsistemas.contacto@gmail.com>
2021-12-22 12:41:58 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             MI PAGINA WEB
           </a>
-        </div>
-      </nav>
-
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
         </div>
       </nav>
       <div className="container">
