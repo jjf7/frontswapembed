10b9c7599d2a1fdb2dfc4ae83d9bf94adfee7da8
jjf7 <jfsistemas.contacto@gmail.com>
2021-12-22 12:39:52 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">Texto</div>
-        <div className="col-md-6">Swap</div>
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            MI PAGINA WEB
+          </a>
+        </div>
+      </nav>
+
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">Texto</div>
+          <div className="col-md-6">Swap</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
